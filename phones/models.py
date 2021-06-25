7a6add66d6b735fa30de30406af3e4f35f5643ca
savraska <balakireva_maria@bk.ru>
2021-06-25 20:37:47 +0500

    добавлен слаг

--- phones/models.py
+++ phones/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
-
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 class Phone(models.Model):
 
     name = models.TextField()
     price = models.IntegerField()
     image = models.ImageField()
     release_date = models.TextField()
-    slug = models.BooleanField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=30, unique=True)
 
 def __str__(self):
-    return
+    return self.name
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Phone.filter(slug=slug).order_by('id')
+    return slug
--- phones/views.py
+++ phones/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from work_with_database.phones.models import Phone
 phones_list =[]
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.object.all()
+    context = {phones}
     return render(request, template, context)
 
 
-def show_product(request, slug):
+def show_product(request, post_slug):
     template = 'product.html'
-    phones = Phone.objects.all()
-    if phones.filter(slug=True):
-        phones_list.append(phones)
-    context = {'phone': phones_list}
+    phones_exist = Phone.objects.filter(lte_exists=True)
+    if phones_exist:
+        info = get_object_or_404(Phone, slug=post_slug)
+        phones_list.append(info)
+        context = {'info': phones_list}
+    else:
+        return HttpResponse('page_not_found')
     return render(request, template, context)
